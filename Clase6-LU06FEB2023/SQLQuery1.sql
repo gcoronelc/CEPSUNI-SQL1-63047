b0480d4d771fee107e174df9ca9dc11daed7cbe7
Gustavo Corone <gcoronelc@gmail.com>
2023-02-06 19:57:09 -0500

    Clase 6

---
+++ Clase6-LU06FEB2023/SQLQuery1.sql
@@ -0,0 +1,20 @@
+-- Planilla por cargo
+select 
+	idcargo cargo,
+	sum(sueldo) planilla
+from RH..empleado 
+group by idcargo
+go
+
+-- La planilla por cargo, pero las que son mayores a 30,000.00
+select 
+	idcargo cargo,
+	sum(sueldo) planilla
+from RH..empleado 
+group by idcargo
+having sum(sueldo) > 30000.00;;
+go
+
+-- Planilla con sueldo y comision
+-- Solución de Juan Escobedo
+select 	idcargo cargo,	sum(sueldo) planilla_sin_comision,	sum(sueldo + COALESCE(comision,0)) planilla_con_comisionfrom RH..empleado group by idcargo;go
